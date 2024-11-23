3eca30a30c5ff201425cab102e2e3dbed0cacb92
absoftlab99 <abdullahtech2@outlook.com>
2024-11-23 21:21:49 +0600

    header top and header complete

--- src/components/Common/Header.tsx
+++ src/components/Common/Header.tsx
@@ -1,9 +1,94 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
+import logo from '../../app/assets/logo.png';
+import { TbBox, TbAppWindow, TbChevronDown, TbHome, TbLayoutGrid, TbLogout, TbMenu2, TbX, TbBrandMeta, TbVectorBezier, TbMovie, TbCode, TbServerCog, TbServer, TbLink, TbAddressBook } from 'react-icons/tb';
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className='grid md:place-content-center'>
+            <div className="lg:w-[1200px] xl:w-[1440px]">
+                <div className="navbar bg-base-100">
+                    <div className="navbar-start lg:gap-6">
+                        <Link href='/' className="flex items-center gap-3">
+                            <figure>
+                                <Image src={logo} width={230} height={100} alt='logo image'></Image>
+                            </figure>
+                        </Link>
+                    </div>
+                    <div className="navbar-center hidden lg:flex">
+                        <ul className="menu menu-horizontal px-1 text-[18px]">
+                            <li><Link href='/'><TbHome></TbHome> Home</Link></li>
+                            <li className="dropdown dropdown-hover">
+                                <div tabIndex={0} role="button" className="text-[18px]"> <TbLayoutGrid></TbLayoutGrid> Services <TbChevronDown></TbChevronDown></div>
+                                <ul tabIndex={0} className="dropdown-content menu rounded-box z-[1] w-52 p-2 sweet-shad ms-0 hover:bg-none">
+                                    <li><Link className='hover:ps-7 transition-all duration-500 hover:text-primary' href='/'><TbAppWindow></TbAppWindow> Web Development</Link></li>
+                                    <li><Link className='hover:ps-7 transition-all duration-500 hover:text-primary' href='/'><TbBox></TbBox> App Development</Link></li>
+                                    <li><Link className='hover:ps-7 transition-all duration-500 hover:text-primary' href='/'><TbBrandMeta></TbBrandMeta> Digital Marketing</Link></li>
+                                    <li><Link className='hover:ps-7 transition-all duration-500 hover:text-primary' href='/'><TbVectorBezier></TbVectorBezier> Graphics Design</Link></li>
+                                    <li><Link className='hover:ps-7 transition-all duration-500 hover:text-primary' href='/'><TbMovie></TbMovie> Video Production</Link></li>
+                                </ul>
+                            </li>
+                            <li><Link href='/'><TbCode></TbCode> Products</Link></li>
+                            <li className="dropdown dropdown-hover">
+                                <div tabIndex={0} role="button" className="text-[18px]"><TbServerCog></TbServerCog> Domain & Hosting <TbChevronDown></TbChevronDown></div>
+                                <ul tabIndex={0} className="dropdown-content menu rounded-box z-[1] w-52 p-2 sweet-shad ms-0 hover:bg-none">
+                                    <li><Link href='/' className='hover:ps-7 transition-all duration-500 hover:text-primary'><TbServer></TbServer> Web Hosting</Link></li>
+                                    <li><Link href='/' className='hover:ps-7 transition-all duration-500 hover:text-primary'><TbLink></TbLink> Search Domain</Link></li>
+                                </ul>
+                            </li>
+                            <li><Link href='/'><TbAddressBook></TbAddressBook> Contact Us</Link></li>
+                        </ul>
+                    </div>
+                    <div className="navbar-end">
+                        <a className="btn btn-primary hidden lg:flex"><TbLogout></TbLogout> Our Product</a>
+                        <label htmlFor="my-drawer" className='btn btn-ghost lg:hidden text-[28px]'><TbMenu2></TbMenu2></label>
+                    </div>
+                </div>
+            </div>
+
+            {/* Drawer content setcion */}
+            <div className="drawer">
+                <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-side">
+                    <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+                    <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+                        <div className="flex justify-between items-center">
+                            <button type="button" className='btn btn-sm btn-primary'>Our Products</button>
+                            <label htmlFor="my-drawer" aria-label="close sidebar" className="btn btn-sm btn-ghost text-[28px]"><TbX></TbX></label>
+                        </div>
+                        <div className="divider my-0"></div>
+                        <li><Link href='/'><TbHome></TbHome> Home</Link></li>
+                        <li>
+                            <details open>
+                                <summary>
+                                    <TbLayoutGrid></TbLayoutGrid> Services
+                                </summary>
+                                <ul>
+                                    <li><Link href='/'><TbAppWindow></TbAppWindow> Web Development</Link></li>
+                                    <li><Link href='/'><TbBox></TbBox> App Development</Link></li>
+                                    <li><Link href='/'><TbBrandMeta></TbBrandMeta> Digital Marketing</Link></li>
+                                    <li><Link href='/'><TbVectorBezier></TbVectorBezier> Graphics Design</Link></li>
+                                    <li><Link href='/'><TbMovie></TbMovie> Video Production</Link></li>
+                                </ul>
+                            </details>
+                        </li>
+                        <li><Link href='/'><TbCode></TbCode> Products</Link></li>
+                        <li>
+                            <details open>
+                                <summary>
+                                    <TbServerCog></TbServerCog> Domain & Hosting
+                                </summary>
+                                <ul>
+                                    <li><Link href='/'><TbServer></TbServer> Web Hosting</Link></li>
+                                    <li><Link href='/'><TbLink></TbLink> Search Domain</Link></li>
+                                </ul>
+                            </details>
+                        </li>
+                        <li><Link href='/'><TbAddressBook></TbAddressBook> Contact Us</Link></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Common/HeaderTop.tsx
+++ src/components/Common/HeaderTop.tsx
@@ -3,9 +3,9 @@ import { TbMessage2, TbPhoneCall, TbRosetteDiscountCheckFilled } from 'react-ico
 
 const HeaderTop = () => {
     return (
-        <div className='grid md:place-content-center bg-primary h-8'>
+        <div className='grid md:place-content-center bg-primary h-8 px-4 md:px-0'>
             <div className='lg:w-[1200px] xl:w-[1440px]'>
-                <div className="flex justify-between text-white">
+                <div className="justify-between text-white hidden md:flex">
                     <div className="flex gap-4">
                         <div className="flex gap-2 items-center">
                             <TbMessage2></TbMessage2>
@@ -15,6 +15,20 @@ const HeaderTop = () => {
                             <TbRosetteDiscountCheckFilled></TbRosetteDiscountCheckFilled>
                             <p>Offers</p>
                         </div>
+                    </div>
+                    <div className="flex gap-2 items-center">
+                        <TbPhoneCall></TbPhoneCall>
+                        <p>Call Now</p>
+                    </div>
+                </div>
+                <div className="flex justify-between items-center h-8 text-white text-[16px] md:hidden">
+                    <div className="flex gap-2 items-center">
+                        <TbMessage2></TbMessage2>
+                        <p>Live Chat</p>
+                    </div>
+                    <div className="flex gap-2 items-center">
+                        <TbRosetteDiscountCheckFilled></TbRosetteDiscountCheckFilled>
+                        <p>Offers</p>
                     </div>
                     <div className="flex gap-2 items-center">
                         <TbPhoneCall></TbPhoneCall>
